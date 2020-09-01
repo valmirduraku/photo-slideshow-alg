a9b9eb9f7067b4b9d38da2e6d94acd668b50ce16
Valmir <valmirduraku@gmail.com>
2020-09-01 23:42:31 +0200

    Updates

---
+++ precache-manifest.8dd2e863957406de1e23d1b28343263c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b566e3f8de2c789ad73a4e6ffaea0bfc",
+    "url": "/photo-slideshow-alg/index.html"
+  },
+  {
+    "revision": "d192e3b1d14f1a427d74",
+    "url": "/photo-slideshow-alg/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "7ca96ff5229c1674c94b",
+    "url": "/photo-slideshow-alg/static/css/main.c45fa8c4.chunk.css"
+  },
+  {
+    "revision": "d192e3b1d14f1a427d74",
+    "url": "/photo-slideshow-alg/static/js/2.cac9d209.chunk.js"
+  },
+  {
+    "revision": "bdfdaee5779fe0c6a048c3619e9bf199",
+    "url": "/photo-slideshow-alg/static/js/2.cac9d209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ca96ff5229c1674c94b",
+    "url": "/photo-slideshow-alg/static/js/main.fa45811f.chunk.js"
+  },
+  {
+    "revision": "a2e2b1b74bbf1e66f120",
+    "url": "/photo-slideshow-alg/static/js/runtime-main.f98f66ab.js"
+  },
+  {
+    "revision": "834661d195cb64c84bb356bd07412582",
+    "url": "/photo-slideshow-alg/static/media/User.834661d1.png"
+  }
+]);
